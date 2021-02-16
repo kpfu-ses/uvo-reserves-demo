6bc6342687778ea6d79cc2f3589b2d9748194757
Sumbel <sumbel.enikeeva@gmail.com>
2021-02-16 10:36:37 +0300

    creating coords files

--- app/microservices/first.py
+++ app/microservices/first.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from pathlib import Path
+import json
 
 from flask import current_app
 
@@ -8,6 +9,19 @@ from app import db
 from app.helpers.parser import read_strat
 from app.models import Stratigraphy
 from app.modules.first.Strat_P2ss2 import get_strat
+
+
+def create_coords_files(wells, run_id):
+    for well in wells:
+        for coords in well.coords():
+            Path(
+                current_app.config['SERVICES_PATH'] + 'first/' + str(run_id) + '/input_data/coords/' + well.name).mkdir(
+                parents=True, exist_ok=True)
+            coords_json = {"Well": coords.well_id, "X": coords.x, "Y": coords.y, "RKB": coords.rkb}
+            filename = current_app.config['SERVICES_PATH'] + 'first/' + str(run_id) + \
+                       '/input_data/coords/' + well.name + '/' + coords.filepath
+            with open(filename, 'w') as f:
+                json.dump(coords_json, f, indent=4)
 
 
 def save_strat(wells, run_id):
@@ -23,18 +37,12 @@ def save_strat(wells, run_id):
 
 
 def run_first(wells, run_id):
-    Path(current_app.config['SERVICES_PATH'] + 'first/' + str(run_id) + '/output_data/').mkdir(parents=True,                                                                                          exist_ok=True)
+    Path(current_app.config['SERVICES_PATH'] + 'first/' + str(run_id) + '/output_data/').mkdir(parents=True,
+                                                                                               exist_ok=True)
     Path(current_app.config['SERVICES_PATH'] + 'first/' + str(run_id) + '/output_data/Report.txt').touch(exist_ok=True)
-
+    create_coords_files(wells, run_id)
     for well in wells:
-        for coords in well.coords():
-            # os.system("mkdir ../modules/1/input_data/coords/" + well.name)
-            Path(
-                current_app.config['SERVICES_PATH'] + 'first/' + str(run_id) + '/input_data/coords/' + well.name).mkdir(
-                parents=True, exist_ok=True)
-            shutil.copyfile(os.path.join(current_app.config['UPLOAD_FOLDER'], coords.filepath),
-                            current_app.config['SERVICES_PATH'] + 'first/' + str(
-                                run_id) + '/input_data/coords/' + well.name + '/' + coords.filepath)
+
         for logs in well.logs():
             Path(current_app.config['SERVICES_PATH'] + 'first/' + str(
                 run_id) + '/input_data/wellLogs/' + well.name).mkdir(parents=True, exist_ok=True)
