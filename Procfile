5978945dbfcae1d6358467911e7b2fcf4c77abb4
Sumbel <sumbel.enikeeva@gmail.com>
2021-04-07 11:07:46 +0300

    gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: flask db upgrade;
+web: flask db upgrade; gunicorn uvo-reserves-demo:app
--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:7c056266@localhost:5432/uvo_reserves'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql+psycopg2://postgres:7c056266@localhost:5432/uvo_reserves'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'uploads'
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
