2dcf0367ea60a04cd43e42aa81bb821a7107a310
Айнур Гимадеев <inuur@mail.ru>
2021-05-25 20:53:51 +0300

    curves api changes

--- app/api/crvs.py
+++ app/api/crvs.py
@@ -9,9 +9,13 @@ from app.models import CoreResults, Well
 # результаты модуля увязки с керн
 @bp.route('/runs/<run_id>/<well_id>', methods=['GET'])
 @jwt_required(locations=['cookies'])
-@user_run_access()
+# @user_run_access()
 def core_view(run_id, well_id):
     well = Well.query.get(well_id)
     core_res = CoreResults.query.filter_by(run_id=run_id, well_id=well_id).first()
-    return jsonify({'well': well.name,
-                    'data': core_res.data})
+    return jsonify(
+        {
+            'well': well.name,
+            'data': core_res.data
+        }
+    )
